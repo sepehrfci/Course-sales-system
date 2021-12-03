03caa2f2d8a3610ee87271a4a7f008a3117a219c
sepehrfci <sepehrfci@gmail.com>
2021-12-03 21:15:09 +0330

    add farsi lang

--- resources/views/myauth/register.blade.php
+++ resources/views/myauth/register.blade.php
@@ -1,22 +1,40 @@
 @extends('myauth.master')
 @section('title','صفحه ثبت نام')
 @section('content')
-    <form action="" class="form" method="post">
+    <form class="form" method="POST" action="{{ route('register') }}">
+        @csrf
         <a class="account-logo" href="index.html">
             <img src="img/weblogo.png" alt="">
         </a>
         <div class="form-content form-account">
-            <input type="text" class="txt" placeholder="نام و نام خانوادگی">
-            <input type="text" class="txt txt-l" placeholder="ایمیل">
-            <input type="text" class="txt txt-l" placeholder="شماره موبایل">
-            <input type="text" class="txt txt-l" placeholder="رمز عبور">
-            <span class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
+            <input id="name" name="name" type="text" class="txt" value="{{ old('name') }}"
+                   placeholder="نام و نام خانوادگی" required autofocus >
+            @error('name')
+                <p>{{ $message }}</p>
+            @enderror()
+            <input id="email" name="email" type="email" value="{{ old('email') }}" class="txt txt-l"
+                   placeholder="ایمیل" required>
+            @error('email')
+            <p>{{ $message }}</p>
+            @enderror()
+            <input id="phone" name="phone" type="text" value="{{ old('phone') }}" class="txt txt-l"
+                   placeholder="شماره موبایل">
+            @error('phone')
+            <p>{{ $message }}</p>
+            @enderror()
+            <input id="password" name="password" type="password" class="txt txt-l" placeholder="رمز عبور" required>
+            <input id="password_confirmation" name="password_confirmation" type="password" class="txt txt-l" placeholder="تکرار رمز عبور" required>
+            <span class="rules">
+                رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای غیر الفبا مانند !@#$%^&*() باشد.</span>
             <br>
+            @error('password')
+            <p>{{ $message }}</p>
+            @enderror()
             <button class="btn continue-btn">ثبت نام و ادامه</button>
 
         </div>
         <div class="form-footer">
-            <a href="login.html">صفحه ورود</a>
+            <a href="{{ route('login') }}">صفحه ورود</a>
         </div>
     </form>
 @endsection
