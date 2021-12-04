9a6e3899e4e0d12a6f62aef0c9b318ca0bd55437
sepehrfci <sepehrfci@gmail.com>
2021-12-05 00:28:57 +0330

    modify namespace and delete ValidPhone.php
    modify RegisteredUserController.php namespace and delete app/Rules/ValidPhone.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use App\Rules\ValidPhone;
+use Sepehrfci\User\Rules\ValidPhone;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
