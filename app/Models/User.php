33d51ed834951a30a00ad366047689a64ae66631
sepehrfci <sepehrfci@gmail.com>
2021-12-04 11:50:43 +0330

    add verified email

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,7 +37,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  * @mixin \Eloquent
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
--- resources/views/myauth/verify-email.blade.php
+++ resources/views/myauth/verify-email.blade.php
@@ -1,24 +1,35 @@
 @extends('myauth.master')
 @section('title','صفحه تایید ایمیل')
 @section('content')
-    <form action="" class="form" method="post">
+    <div action="" class="form" method="post">
         <a class="account-logo" href="index.html">
-        <img src="img/weblogo.png" alt="">
+        <img src="img/weblogo.png" alt="logo">
         </a>
         <div class="card-header">
-            <p class="activation-code-title">کد فرستاده شده به ایمیل  <span>Mohammadniko3@gmail.com</span>
-                را وارد کنید . ممکن است ایمیل به پوشه spam فرستاده شده باشد
+            @if (session('status') == 'verification-link-sent')
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    ایمیل جدید ارسال شد
+                </div>
+            @endif
+            <p class="activation-code-title">بر روی لینک فرستاده شده به ایمیل  <span>{{ \Illuminate\Support\Facades\Auth::user()->email }}</span>
+                کلیک کنید . ممکن است ایمیل به پوشه spam فرستاده شده باشد
             </p>
         </div>
-        <div class="form-content form-content1">
-            <input class="activation-code-input" placeholder="فعال سازی">
-            <br>
-            <button class="btn i-t">تایید</button>
-        </div>
-        <div class="form-footer">
-            <a href="login.html">صفحه ثبت نام</a>
-        </div>
-    </form>
+        <form method="POST" action="{{ route('verification.send') }}">
+            @csrf
+            <div class="form-content form-content1">
+                <button type="submit" class="btn btn-recoverpass i-t">ارسال مجدد لینک فعال سازی</button>
+            </div>
+        </form>
+            <div class="form-footer">
+                @if(auth()->user())
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="btn" type="submit">خروج از سیستم</button>
+                    </form>
+                @endif
+            </div>
+    </div>
 @endsection
 
 
