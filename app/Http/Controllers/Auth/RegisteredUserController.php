6e128356bd544c11c988b20bf7585e66814f6d04
sepehrfci <sepehrfci@gmail.com>
2021-12-05 10:51:58 +0330

    transfer user model and change namespaces
    special change in config/auth.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use Sepehrfci\User\Models\User;
 use App\Providers\RouteServiceProvider;
 use Sepehrfci\User\Rules\ValidPhone;
 use Illuminate\Auth\Events\Registered;
