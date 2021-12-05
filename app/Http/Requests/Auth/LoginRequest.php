9206b19f54b0c36caff76268db0a463074fbf75a
sepehrfci <sepehrfci@gmail.com>
2021-12-05 11:31:42 +0330

    Change the namespace in LoginRequest.php
    app/Http/Requests/Auth/LoginRequest.php

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
-use App\Models\User;
+use Sepehrfci\User\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
