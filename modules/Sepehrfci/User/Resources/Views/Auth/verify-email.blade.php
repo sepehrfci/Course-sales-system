e6f89a313ffe36432e4bf1caeb11979bd86788ca
sepehrfci <sepehrfci@gmail.com>
2021-12-09 00:49:13 +0330

    Update the verify-email.blade.php
    modules/Sepehrfci/User/Resources/Views/Auth/verify-email.blade.php

--- modules/Sepehrfci/User/Resources/Views/Auth/verify-email.blade.php
+++ modules/Sepehrfci/User/Resources/Views/Auth/verify-email.blade.php
@@ -1,8 +1,8 @@
 @extends('User::Auth.master')
 @section('title','صفحه تایید ایمیل')
 @section('content')
-    <div action="" class="form" method="post">
-        <a class="account-logo" href="index.html">
+    <div class="form" >
+        <a class="account-logo" href="/">
         <img src="img/weblogo.png" alt="logo">
         </a>
         <div class="card-header">
@@ -11,25 +11,42 @@
                     ایمیل جدید ارسال شد
                 </div>
             @endif
-            <p class="activation-code-title">بر روی لینک فرستاده شده به ایمیل  <span>{{ \Illuminate\Support\Facades\Auth::user()->email }}</span>
-                کلیک کنید . ممکن است ایمیل به پوشه spam فرستاده شده باشد
+            <p class="activation-code-title">کد فرستاده شده به ایمیل  <span>{{ auth()->user()->email }}</span>
+                را در کادر زیر وارد کنید . ممکن است ایمیل به پوشه spam فرستاده شده باشد
             </p>
-        </div>
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-            <div class="form-content form-content1">
-                <button type="submit" class="btn btn-recoverpass i-t">ارسال مجدد لینک فعال سازی</button>
-            </div>
-        </form>
-            <div class="form-footer">
-                @if(auth()->user())
-                    <form action="{{ route('logout') }}" method="POST">
+                <div class="form-content form-content1">
+                    <form action="" method="POST">
                         @csrf
-                        <button class="btn" type="submit">خروج از سیستم</button>
+                        <input name="verify_code" class="activation-code-input" placeholder="فعال سازی">
+                        @error('verify_code')
+                            <br>
+                            <strong> {{ $message }}</strong>
+                        @enderror
+                        <br>
+                        <button class="btn i-t">تایید</button>
                     </form>
-                @endif
-            </div>
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+                        <div class="form-content">
+                            <button type="submit" class="btn btn-recoverpass i-t">ارسال مجدد لینک فعال سازی</button>
+                        </div>
+                    </form>
+                </div>
+
+        </div>
+        <div class="form-footer">
+            @if(auth()->user())
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn" type="submit">خروج از سیستم</button>
+                </form>
+            @endif
+        </div>
+
     </div>
 @endsection
-
+@section('js')
+    <script src="/js1/jquery-3.4.1.min.js"></script>
+    <script src="/js1/activation-code.js"></script>
+@endsection
 
