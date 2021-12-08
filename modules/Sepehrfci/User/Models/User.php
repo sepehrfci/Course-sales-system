963c2542a56d5f98b3cb0fc1fd64751bdb4f1fd8
sepehrfci <sepehrfci@gmail.com>
2021-12-08 23:00:49 +0330

    create sendEmailVerificationNotification() method
    modules/Sepehrfci/User/Models/User.php

--- modules/Sepehrfci/User/Models/User.php
+++ modules/Sepehrfci/User/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Sepehrfci\User\Notifications\VerifyEmail;
 
 /**
  * App\Models\User
@@ -69,4 +70,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmail());
+    }
 }
