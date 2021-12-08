67e7e28d3acc1d743076740d4a8be346097e092d
sepehrfci <sepehrfci@gmail.com>
2021-12-09 00:50:04 +0330

    Add getCache() and deleteCache() methods in VerifyCodeService.php
    modules/Sepehrfci/User/Services/VerifyCodeService.php

--- modules/Sepehrfci/User/Services/VerifyCodeService.php
+++ modules/Sepehrfci/User/Services/VerifyCodeService.php
@@ -3,6 +3,8 @@
 namespace Sepehrfci\User\Services;
 
 use Exception;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\SimpleCache\InvalidArgumentException;
 
 class VerifyCodeService
@@ -27,7 +29,31 @@ class VerifyCodeService
                 now()->addMinutes(2)
             );
         } catch (InvalidArgumentException $e) {
-            echo "Error";
+            dd('ERROR');
+        }
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @throws Exception
+     */
+    public static function getCache($id)
+    {
+        try {
+            return cache()->get('verify_email_' . $id);
+        }
+        catch (NotFoundExceptionInterface | ContainerExceptionInterface $e) {
+            echo "ERROR";
+        }
+    }
+
+    public static function deleteCache($id)
+    {
+        try {
+            return cache()->delete('verify_email_' . $id);
+        }
+        catch (InvalidArgumentException | Exception $e) {
+            echo "ERROR";
         }
     }
 }
