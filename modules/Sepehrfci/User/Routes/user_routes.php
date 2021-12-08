95e06531baaef6364e0682ec42b8d82ba7f751ac
sepehrfci <sepehrfci@gmail.com>
2021-12-09 00:48:44 +0330

    Add verify-email route to user_routes.php
    modules/Sepehrfci/User/Routes/user_routes.php

--- modules/Sepehrfci/User/Routes/user_routes.php
+++ modules/Sepehrfci/User/Routes/user_routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Sepehrfci\User\Http\Controllers\Auth\VerifyEmailController;
 
 Route::group(['namespace' => 'Sepehrfci\User\Http\Controllers','middleware' => 'web'], function (){
 
@@ -9,4 +10,6 @@ Route::group(['namespace' => 'Sepehrfci\User\Http\Controllers','middleware' => '
     })->middleware(['auth','verified'])->name('dashboard');
 
     require __DIR__.'/auth.php';
+
+    Route::post('verify-email',[VerifyEmailController::class,'verify'])->name('verification.verify');
 });
