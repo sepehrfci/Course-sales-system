2bb8ce890a308a9be5c2b95e006895d0bf7b2aee
sepehrfci <sepehrfci@gmail.com>
2021-12-12 01:33:06 +0330

    Create Category model
    modules/Sepehrfci/Category/Models/Category.php

---
+++ modules/Sepehrfci/Category/Models/Category.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sepehrfci\Category\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property mixed $parent
+ * @property mixed $parent_id
+ * @property mixed $parentCategory
+ */
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function parentCategory()
+    {
+        return $this->belongsTo(Category::class,'parent_id');
+    }
+
+    public function subCategories()
+    {
+        return $this->hasMany(Category::class,'parent_id');
+    }
+
+    public function getParentAttribute()
+    {
+        return is_null($this->parent_id) ? 'ندارد' : $this->parentCategory->title;
+    }
+
+
+}
