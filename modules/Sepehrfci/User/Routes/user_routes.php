b49deb9295b2e8b88e5c658d05e9515dcc53df68
sepehrfci <sepehrfci@gmail.com>
2021-12-11 17:40:35 +0330

    Delete dashboard route in user_routes.php
    modules/Sepehrfci/User/Routes/user_routes.php

--- modules/Sepehrfci/User/Routes/user_routes.php
+++ modules/Sepehrfci/User/Routes/user_routes.php
@@ -5,10 +5,6 @@ use Sepehrfci\User\Http\Controllers\Auth\VerifyEmailController;
 
 Route::group(['namespace' => 'Sepehrfci\User\Http\Controllers','middleware' => 'web'], function (){
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth','verified'])->name('dashboard');
-
     require __DIR__.'/auth.php';
 
     Route::post('verify-email',[VerifyEmailController::class,'verify'])->name('verification.verify');
