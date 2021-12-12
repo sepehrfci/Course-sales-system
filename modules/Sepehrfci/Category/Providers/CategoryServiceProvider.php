057234119cebb1dd92edd2965ee98d6263dac1ba
sepehrfci <sepehrfci@gmail.com>
2021-12-13 00:25:25 +0330

    Add sidebar items to Sidebar config

--- modules/Sepehrfci/Category/Providers/CategoryServiceProvider.php
+++ modules/Sepehrfci/Category/Providers/CategoryServiceProvider.php
@@ -5,10 +5,21 @@ use Illuminate\Support\ServiceProvider;
 
 class CategoryServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/categories_routes.php');
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views/','Category');
         $this->loadMigrationsFrom(__DIR__ .'/../Database/Migrations/');
+    }
+
+    public function boot()
+    {
+        $this->app->booted(function (){
+            config()->set('sidebar.items.categories',[
+                'title' => 'دسته بندی ها',
+                'url' => route('categories.index'),
+                'icon' => 'i-categories'
+            ]);
+        });
     }
 }
