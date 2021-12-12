a9695be10928c68cb1600a738453ea5a85946cd0
sepehrfci <sepehrfci@gmail.com>
2021-12-12 23:39:44 +0330

    Refactor the CategoryRepository.php
    modules/Sepehrfci/Category/Repository/CategoryRepository.php

--- modules/Sepehrfci/Category/Repository/CategoryRepository.php
+++ modules/Sepehrfci/Category/Repository/CategoryRepository.php
@@ -10,24 +10,29 @@ class CategoryRepository
     {
         return Category::all();
     }
+
     public function allExceptById($id)
     {
         return Category::all()->filter(function ($item) use($id){
             return $item->id != $id;
         });
     }
+
     public function findById($id)
     {
         return Category::query()->findOrFail($id);
     }
+
     public function store($request)
     {
         Category::query()->create($request->all());
     }
+
     public function update($id,$request)
     {
         Category::query()->findOrFail($id)->update($request->all());
     }
+
     public function delete($id)
     {
         Category::query()->findOrFail($id)->delete();
