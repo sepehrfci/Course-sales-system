a090422a75d33aab384d06589ae981122d784260
sepehrfci <sepehrfci@gmail.com>
2021-12-13 22:04:21 +0330

    Update the  categories_routes.php
    modules/Sepehrfci/Category/Routes/categories_routes.php

--- modules/Sepehrfci/Category/Routes/categories_routes.php
+++ modules/Sepehrfci/Category/Routes/categories_routes.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Sepehrfci\Category\Http\Controllers\CategoryController;
 
-Route::prefix('dashboard')->namespace('Sepehrfci\Category\Http\Controllers')
-    ->middleware(['web','auth','verified'])->group(function (){
+Route::prefix('dashboard')->middleware(['web','auth','verified'])->group(function (){
 
-    Route::resource('categories','CategoryController');
+    Route::resource('categories',CategoryController::class);
 
 });
