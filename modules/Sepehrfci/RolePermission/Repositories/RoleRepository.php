055dccb099dc337d520d176d534806193878de6a
Sepehr Fassihi <sepehrfci@gmail.com>
2021-12-16 23:37:42 +0330

    Add two methods to RoleRepository.php
    modules/Sepehrfci/RolePermission/Repositories/RoleRepository.php

--- modules/Sepehrfci/RolePermission/Repositories/RoleRepository.php
+++ modules/Sepehrfci/RolePermission/Repositories/RoleRepository.php
@@ -11,8 +11,23 @@ class RoleRepository
         return Role::all();
     }
 
+    public function findById($id)
+    {
+        return Role::query()->findOrFail($id);
+    }
+
+    public function update($request,$role)
+    {
+        return $role->syncPermissions($request->permissions)->update(['name' => $request->name]);
+    }
+
     public function create($request)
     {
         return Role::create(['name' => $request->name])->syncPermissions($request->permissions);
+    }
+
+    public function delete($id)
+    {
+        return Role::query()->findOrFail($id)->delete();
     }
 }
