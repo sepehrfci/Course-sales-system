3725d3d3cd5fd13fdf76746364cf276f555b7c03
Sepehr Fassihi <sepehrfci@gmail.com>
2021-12-17 23:58:12 +0330

    Update the role_permission_routes.php
    modules/Sepehrfci/RolePermission/Routes/role_permission_routes.php

--- modules/Sepehrfci/RolePermission/Routes/role_permission_routes.php
+++ modules/Sepehrfci/RolePermission/Routes/role_permission_routes.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Sepehrfci\RolePermission\Http\Controllers\PermissionController;
 use Sepehrfci\RolePermission\Http\Controllers\RolePermissionController;
 
 Route::prefix('dashboard')->middleware(['web','auth','verified'])->group(function () {
     Route::resource('roles-permissions',RolePermissionController::class);
+    Route::post('roles-permissions/permissions/store',[PermissionController::class,'store'])
+        ->name('permissions.store');
 });
