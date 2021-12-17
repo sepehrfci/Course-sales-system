4d7a9ed028cd2ef181263bedcd6bc36de3fb1694
Sepehr Fassihi <sepehrfci@gmail.com>
2021-12-17 23:57:27 +0330

    Create the PermissionRequest.php
    modules/Sepehrfci/RolePermission/Http/Requests/PermissionRequest.php

---
+++ modules/Sepehrfci/RolePermission/Http/Requests/PermissionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sepehrfci\RolePermission\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required','min:4','unique:permissions,name'],
+            'name_fa' => ['required','min:4'],
+        ];
+    }
+}
